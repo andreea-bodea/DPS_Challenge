d82e023e322e85ef1faf8ef537bd1a61154f8196
andreea-bodea <58235076+andreea-bodea@users.noreply.github.com>
2023-12-08 00:29:20 +0100

    Updated with pickel

--- app.py
+++ app.py
@@ -4,24 +4,12 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from flask import Flask, request, jsonify
+import pickle
+from datetime import datetime
 
 app = Flask(__name__)
 
-df_initial = pd.read_csv('monatszahlen2307_verkehrsunfaelle_10_07_23_nosum.csv')
-
-df = df_initial[df_initial['JAHR'] <= 2020]
-df = df.reset_index(drop=True)
-df = df[['MONATSZAHL', 'AUSPRAEGUNG', 'MONAT', 'WERT']]
-df['MONATSZAHL'] = df['MONATSZAHL'].astype("string")
-df['AUSPRAEGUNG'] = df['AUSPRAEGUNG'].astype("string")
-df['MONAT'] = df['MONAT'].astype("string")
-df['MONAT'] = pd.to_datetime(df['MONAT'].astype("string"), format='%Y%m', errors='coerce')
-df = df.sort_values(by='MONAT')
-df = df.reset_index(drop=True)
-
-filtered_df = df[(df['MONATSZAHL'] == 'Alkoholunfälle') & (df['AUSPRAEGUNG'] == 'insgesamt')]
-filtered_df['MONTH_NUMERIC'] = (filtered_df['MONAT'] - filtered_df['MONAT'].min()) / np.timedelta64(1, 'M')
-
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -30,17 +18,9 @@ def predict():
     year = data.get('year')
     month = data.get('month')
 
-    input_month_numeric = (pd.to_datetime(f'{year}-{month:02d}') - df['YearMonth'].min()) / np.timedelta64(1, 'M')
+    input_days_numeric = (pd.to_datetime(f'{year}-{month}') - datetime(2000, 1, 1)).days
 
-    X_train, X_test, y_train, y_test = train_test_split(
-    filtered_df[['MONTH_NUMERIC']],
-    filtered_df['WERT'],
-    test_size = 0.2,  
-    random_state = 42  # seed for reproducibility
-    )
-
-    model = LinearRegression()
-    model.fit(X_train, y_train)
+    input_month_numeric = input_days_numeric / 30  
 
     prediction = model.predict([[input_month_numeric]])[0]
 
